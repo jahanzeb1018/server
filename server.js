85cb9e8e1bc22d58f6497f07c605fa3939294309
jahanzeb1018 <jahanzeb1018@gmail.com>
2025-03-16 14:30:13 +0100

    aa

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ require("dotenv").config();
 const app = express();
 
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://jahanzeb1018.github.io/BCN-REGATTA/",
-  ],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
