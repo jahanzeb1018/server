584f050637705876b0cd09494ccd3bfe19cde2ff
jahanzeb1018 <jahanzeb1018@gmail.com>
2025-03-18 22:43:35 +0100

    Server size json

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 const server = http.createServer(app);
 const io = socketIo(server, {
